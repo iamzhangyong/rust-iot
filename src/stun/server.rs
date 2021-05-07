f07d53fa5a6ddf96256b3f8aeefd8000e4d72a13
zed.zy <zed.zy@antgroup.com>
2021-05-07 19:22:59 +0800

    exec udp server

--- src/stun/server.rs
+++ src/stun/server.rs
@@ -5,10 +5,13 @@ use fibers_global;
 
 pub fn start_stun_server(port: i32) -> Result<(), Error> {
     let addr = format!("0.0.0.0:{}", port).parse().unwrap();
-    UdpServer::start(
+    let server = UdpServer::start(
         fibers_global::handle(),
         addr,
         BindingHandler,
     );
+
+    fibers_global::execute(server);
+
     Ok(())
 }
